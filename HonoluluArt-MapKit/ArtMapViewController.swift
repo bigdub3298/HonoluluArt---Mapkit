ec36fa1a35a6c75c0762779336393ada134cb296
bigdub3298 <worldwidewes@Wesleys-MacBook-Pro.local>
2016-11-21 23:49:37 -0700

    Adds MapView

---
+++ HonoluluArt-MapKit/ArtMapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ArtMapViewController.swift
+//  HonoluluArt-MapKit
+//
+//  Created by Wesley Austin on 11/21/16.
+//  Copyright © 2016 Wesley Austin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ArtMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
